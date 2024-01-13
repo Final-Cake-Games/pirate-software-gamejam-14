c8848b399da16ce4ce29e17c1317f6abcf34ef7a
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-13 10:49:00 +0000

    Added movement logic
    On run add immediate MAX_SPEED. On stop slow down by 20 each _physics_process run. (Smooth stop)

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,10 +12,19 @@ extends CharacterBody2D
 # Input do utilizador (direção no X)
 var direction : float
 
+func _process(delta):
+	direction = Input.get_axis('move_left', 'move_right')
+
 func _physics_process(delta):
 	# Se não está no chão, aplica gravidade
 	if !is_on_floor():
 		velocity.y += GRAVITY * delta
+	
+	# Lógica de andar/parar na !!horizontal!!
+	if direction != 0:  # Estamos a começar a andar
+		velocity.x = direction * MAX_SPEED  # Aplica velocidade máxima imediatamente
+	else:  # Estamos a parar
+		velocity.x = move_toward(velocity.x, 0, 20)  # Abranda até 0 de 20 em 20 unidades
 		
 	move_and_slide()
 
