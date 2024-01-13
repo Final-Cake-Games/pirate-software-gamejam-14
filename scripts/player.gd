b9974cb264001a7e47d87847005ecc84b3e2dd4f
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-13 10:17:04 +0000

    Added gravity to Player

--- scripts/player.gd
+++ scripts/player.gd
@@ -13,7 +13,11 @@ extends CharacterBody2D
 var direction : float
 
 func _physics_process(delta):
-	pass
+	# Se não está no chão, aplica gravidade
+	if !is_on_floor():
+		velocity.y += GRAVITY * delta
+		
+	move_and_slide()
 
 
 
