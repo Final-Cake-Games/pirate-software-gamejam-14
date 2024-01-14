b20be09ffcce458e1709dd78f39686195b773d91
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-14 12:41:15 +0000

    Make tool look at mouse position

--- scripts/plumber_tool.gd
+++ scripts/plumber_tool.gd
@@ -11,6 +11,7 @@ func _process(delta):
 func _physics_process(delta):
 	if picked_up == true:
 		position = get_node('../Player/WeaponPos').global_position
+		look_at(get_global_mouse_position())
 		
 		linear_velocity = Vector2.ZERO
 		
