4993069c0d0cc1dca3963606a085aaad2f18a02b
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-15 16:19:58 +0000

    Update sewage_spread when all valves are closed

--- scripts/test_level.gd
+++ scripts/test_level.gd
@@ -5,15 +5,29 @@ extends Node2D
 
 @onready var player : CharacterBody2D = $Player
 
-var level_valves = []
+var level_valves_arr = []
+var closed_valves_arr = []
+var total_valves_count : int = 0
+var closed_valves_count : int = 0
 var last_seen_valve : Area2D = null
 
+
 func _ready():
-	level_valves = $Valves.get_children()
+	level_valves_arr = $Valves.get_children()
 
 func _process(delta):
-	water_level.is_rising = !level_valves.all(check_all_valves_closed)  # Verifica todas as valvulas do nível para parar agua
+	#Lógica da subida de água e fecho do esgoto
+	water_level.is_rising = !level_valves_arr.all(get_closed_valves)  # Verifica todas as valvulas do nível para parar agua
+	closed_valves_arr = level_valves_arr.filter(get_closed_valves)  # Guarda valvulas fechadas
 	
+	total_valves_count = level_valves_arr.size()
+	closed_valves_count = closed_valves_arr.size()
+	
+	if total_valves_count == closed_valves_count:
+		sewage_source.one_shot = true
+	
+	
+	# Lógia do jogador concertar
 	if player.is_near_valve: 
 		last_seen_valve = player.valve_nearby
 		if !last_seen_valve.is_fixed:
@@ -30,10 +44,6 @@ func toggle_fixing(status : bool):
 	player.fixing = status
 	if last_seen_valve:
 		last_seen_valve.is_getting_fixed = status
-		
-func check_all_valves_closed(valve):
+
+func get_closed_valves(valve):
 	return valve.is_fixed
-		
-	
-	
-		
