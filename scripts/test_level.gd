8f11a1d646b3a8a5b968bdeb4acaf0147487a908
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-15 13:40:33 +0000

    Level checks for player fixing valve
    Delegates the fixing to the valve with is_getting_fixed
     property

--- scripts/test_level.gd
+++ scripts/test_level.gd
@@ -1,2 +1,14 @@
 extends Node2D
 
+@export var sewage_source : CPUParticles2D
+@export var water_level : Area2D
+
+@onready var player : CharacterBody2D = $Player
+
+func _process(delta):
+	
+	if player.is_near_valve:
+		if player.valve_nearby.fix_color == player.can_fix_color && Input.is_action_pressed('fix'):
+			player.valve_nearby.is_getting_fixed = true
+		elif Input.is_action_just_released('fix'):
+			player.valve_nearby.is_getting_fixed = false
