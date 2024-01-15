0031e4eb70f92d0323c41983b5b8ab355928ea94
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-15 15:01:30 +0000

    Fixed issues regarding fixing
    Player was able to fix valve when moving off of area2d.

--- scripts/test_level.gd
+++ scripts/test_level.gd
@@ -5,10 +5,28 @@ extends Node2D
 
 @onready var player : CharacterBody2D = $Player
 
+var last_seen_valve : Area2D = null
+
 func _process(delta):
 	
-	if player.is_near_valve:
-		if player.valve_nearby.fix_color == player.can_fix_color && Input.is_action_pressed('fix'):
-			player.valve_nearby.is_getting_fixed = true
-		elif Input.is_action_just_released('fix'):
-			player.valve_nearby.is_getting_fixed = false
+	if player.is_near_valve: 
+		last_seen_valve = player.valve_nearby
+		if !last_seen_valve.is_fixed:
+			if last_seen_valve.fix_color == player.can_fix_color && Input.is_action_pressed('fix'):
+				toggle_fixing(true)
+			else:
+				toggle_fixing(false)
+		else:
+			toggle_fixing(false)	
+	else:
+		toggle_fixing(false)
+			
+
+func toggle_fixing(status : bool):
+	player.fixing = status
+	if last_seen_valve:
+		last_seen_valve.is_getting_fixed = status
+		
+	
+	
+		
