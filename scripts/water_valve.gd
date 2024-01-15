fb45e7d33dfe60e54465193db4162a022b3e0d6f
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-15 13:43:42 +0000

    Progressable valve closing script
    Valve will now be closed after FIX_TIME has passed of active 'fixing'. Progress is stored as a timer countdown.

---
+++ scripts/water_valve.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+@export var FIX_TIME : float = 10.0
+
+@onready var animation : AnimatedSprite2D = $AnimatedSprite2D
+@onready var progress_time : Timer = $Progress
+
+var fix_color : String = 'RED'
+var is_getting_fixed : bool = false
+var is_fixed : bool = false
+
+func _ready():
+	progress_time.wait_time = FIX_TIME
+	progress_time.start()
+
+func _process(_delta):
+	print(progress_time.time_left)
+	if is_getting_fixed && !is_fixed:
+		get_fixed()
+	else:
+		idle()	
+	
+func get_fixed():
+	if is_fixed:
+		print('valve fixed!')
+		return
+	print('fixing valve...')
+	animation.play('closing')
+	progress_time.paused = false
+	
+func idle():
+	progress_time.paused = true
+	animation.play('default')
+
+func _on_progress_timeout():
+	is_fixed = true
