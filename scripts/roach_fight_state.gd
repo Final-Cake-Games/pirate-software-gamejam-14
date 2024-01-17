15df91592713dc9c6ee511c2678520d19e39622c
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-17 17:59:53 +0000

    Create dmg func

--- scripts/roach_fight_state.gd
+++ scripts/roach_fight_state.gd
@@ -12,6 +12,7 @@ func _enter_state():
 	set_physics_process(true) 
 	animator.play('get_triggered')
 	player = vessel.fight_area.get_overlapping_bodies()[0]
+	do_dmg()
 	
 func _exit_state():
 	set_physics_process(false)
@@ -19,5 +20,8 @@ func _exit_state():
 func _physics_process(delta):
 	pass
 	
+func do_dmg():
+	pass
+
 func _on_fight_range_body_exited(body):
 	player_left_fight_range.emit()
