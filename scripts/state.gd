b6596ca4fe44a0949f33cc1e4d619368a96ff64c
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-17 15:14:52 +0000

    Gave State vessel & animator exports
    To use in any state when necessary.

--- scripts/state.gd
+++ scripts/state.gd
@@ -3,6 +3,9 @@ extends Node
 
 signal state_finished  # Notify manager when curr state is done
 
+@export var vessel : EnemyRoach  # vessel will be occupied only by a scene that extends from EnemyRoach
+@export var animator : AnimationPlayer
+
 func _enter_state() -> void:  # every state load execute the following (gets replaced by each state's _enter_state())
 	pass
 	
