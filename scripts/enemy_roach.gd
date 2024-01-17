b2709f6f01bac44d68e5374625dd7567fa260e3a
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-17 17:19:40 +0000

    Connected all signals to state machine
    Brought sprite flip management to here. Created references for the areas.

--- scripts/enemy_roach.gd
+++ scripts/enemy_roach.gd
@@ -2,7 +2,29 @@ class_name  EnemyRoach
 extends CharacterBody2D
 
 @onready var sprite_2d = $Sprite2D
-@onready var ray_cast_2d = $RayCast2D
+@onready var detection_area = $DetectionRange
+@onready var fight_area = $FightRange
+
+@onready var roach_state_machine = $RoachStateMachine  
+
+@onready var roach_roaming_state = $RoachStateMachine/RoachRoamingState as RoachRoamingState
+@onready var roach_chase_state = $RoachStateMachine/RoachChaseState as RoachChaseState
 
 var ROAM_SPEED = 20
 var CHASE_SPEED = 50
+
+var current_direction : Vector2
+
+func _ready():
+	roach_roaming_state.saw_player.connect(roach_state_machine.change_state.bind(roach_chase_state))
+	roach_chase_state.lost_player.connect(roach_state_machine.change_state.bind(roach_roaming_state))
+
+func _process(delta):
+	current_direction = velocity
+	
+	update_sprite_dir()
+	
+func update_sprite_dir():
+	if current_direction.x != 0:  # Keep sprite facing the right direction
+		sprite_2d.flip_h = (current_direction.x < 0)
+
