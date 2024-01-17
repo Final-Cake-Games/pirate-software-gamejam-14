f1594cff909bfb71ed81037d9042d1de01527bd0
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-17 13:34:35 +0000

    Added change state function to machine
    Also updated state var name to current_state for better understanding of whats going on.

--- scripts/roach_state_machine.gd
+++ scripts/roach_state_machine.gd
@@ -2,11 +2,13 @@
 class_name RoachStateMachine
 extends Node
 
-@export var state : State  # 1st state
+@export var current_state : State  # Only scenes that are of State class or extended from State class 
 
 func _ready():
-	change_state(state)  # Initialize first state on load
+	change_state(current_state)  # Initialize first state on load
 	
 func change_state(new_state : State):
-	pass
-	
+	if current_state != null:  # Exit currently loaded state (if there is any...)
+		current_state._exit_state()
+	new_state._enter_state()
+	current_state = new_state  # Update the current state to the just loaded state
