daa4b7285150293085f890a20d107e0ec658a3be
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-17 19:39:59 +0000

    Pick side to do dmg and return from do dmg when player left

--- scripts/roach_fight_state.gd
+++ scripts/roach_fight_state.gd
@@ -4,12 +4,13 @@ extends State
 signal player_left_fight_range
 
 var player : CharacterBody2D
+var player_has_left : bool 
 
 func _ready():
 	set_physics_process(false)
 	
 func _enter_state():
-	set_physics_process(true) 
+	player_has_left = false
 	animator.play('get_triggered')
 	await animator.animation_finished
 	animator.play('stand_idle')
@@ -19,13 +20,23 @@ func _enter_state():
 func _exit_state():
 	set_physics_process(false)
 
-func _physics_process(delta):
-	pass
 	
 func do_dmg():
-	print('+10 dmg')
+	if (vessel.current_direction.x < 0):
+		animator.play('strike_left')
+	else:
+		animator.play('strike_right')
+		
+	player.take_dmg(10)
+	
+	await animator.animation_finished
+	animator.play('stand_idle')
 	await get_tree().create_timer(3).timeout
-	do_dmg()
+	if player_has_left:
+		return
+	else:
+		do_dmg()
 
 func _on_fight_range_body_exited(body):
+	player_has_left = true
 	player_left_fight_range.emit()
