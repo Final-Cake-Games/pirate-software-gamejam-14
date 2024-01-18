f80fe08d4d8c142922ad6334457e0722967276d7
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-18 11:15:32 +0000

    Check if recently attacked to avoid unfair spam
    Player now gets 3 seconds between getting stroke no matter how many times he enters/exits the fight state of the roach.

--- scripts/roach_fight_state.gd
+++ scripts/roach_fight_state.gd
@@ -4,53 +4,50 @@ extends State
 @onready var reload_timer = $"../../ReloadTimer"
 
 signal player_left_fight_range
+signal roach_die
 
-var player : CharacterBody2D
-var player_has_left : bool
+var recent_attack : bool = false
 
 func _ready():
 	set_physics_process(false)
 	
 func _enter_state():
-	player_has_left = false
-	animator.play('get_triggered')
-	await animator.animation_finished
-	animator.play('stand_idle')
-	player = vessel.fight_area.get_overlapping_bodies()[0]
-	do_dmg()
+	set_physics_process(true) 
+	
+	if recent_attack:
+		reload_timer.paused = false
+	else:
+		do_dmg()
+		reload_timer.start()
 	
 func _exit_state():
 	set_physics_process(false)
-	player = null
-	reload_timer.stop()
 	
-func _process(delta):
-	if vessel.dead:
-		_exit_state()
+	if recent_attack:
+		reload_timer.paused = true
+	else:
+		reload_timer.stop()
+	
+func _physics_process(delta):
+	if !animator.current_animation.begins_with('strike'):
+		animator.play('stand_idle')
+	print(reload_timer.time_left)
 
 func do_dmg():
-	if player_has_left: return
-	
-	reload_timer.stop()
-	reload_timer.wait_time = 3
-	
+	recent_attack = true
+	reload_timer.start()
 	if (vessel.current_direction.x < 0):
 		animator.play('strike_left')
 	else:
 		animator.play('strike_right')
-	
 	await animator.animation_finished
-	
-	player.take_dmg(10)
-	
-	animator.play('stand_idle')
-	
-	reload_timer.start()
-
-func _on_fight_range_body_exited(body):
-	player_has_left = true
-	player_left_fight_range.emit()
+	vessel.player_target.take_dmg(10)
+	#print(vessel.player_target.life)
 
 func _on_reload_timer_timeout():
-	if !player_has_left:
-		do_dmg()
+	recent_attack = false
+	do_dmg()
+
+func _on_fight_range_body_exited(body):
+	if !vessel.dead:
+		player_left_fight_range.emit()
