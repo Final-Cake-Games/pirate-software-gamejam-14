e02a65c91647fa05c966b7e190cf48b5fe34b31b
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-18 15:01:34 +0000

    Game over checker

--- scripts/test_level.gd
+++ scripts/test_level.gd
@@ -10,12 +10,17 @@ var closed_valves_arr = []
 var total_valves_count : int = 0
 var closed_valves_count : int = 0
 var last_seen_valve : Area2D = null
+var level_win : bool = false
+
 
 
 func _ready():
 	level_valves_arr = $Valves.get_children()
 
 func _process(delta):
+	if !water_level.is_rising || player.player_dead:
+		print('game over')
+	
 	#Lógica da subida de água e fecho do esgoto
 	water_level.is_rising = !level_valves_arr.all(get_closed_valves)  # Verifica todas as valvulas do nível para parar agua
 	closed_valves_arr = level_valves_arr.filter(get_closed_valves)  # Guarda valvulas fechadas
