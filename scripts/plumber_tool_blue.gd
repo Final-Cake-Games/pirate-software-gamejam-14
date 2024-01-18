52865563fa335c0d411ca332af929fc151a7dd98
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-18 16:29:40 +0000

    Blue tool logic to work and be able to only wield 1

--- scripts/plumber_tool_blue.gd
+++ scripts/plumber_tool_blue.gd
@@ -15,9 +15,12 @@ func _ready():
 	player = get_node('../Player')
 
 func _process(delta):
+	
 	if can_be_picked_up:
-		if Input.is_action_just_pressed('pick_up') && !player.player_dead:
+		if Input.is_action_just_pressed('pick_up') && !player.player_dead && player.can_fix_color == '':
 			picked_up = true
+			player.tool_count += 1
+			
 
 func _physics_process(delta):
 	can_kill = (linear_velocity.x > 100 || linear_velocity.y > 100 || linear_velocity.x < -100 || linear_velocity.y < -100)
@@ -25,6 +28,7 @@ func _physics_process(delta):
 	if picked_up == true:
 		collision_shape.disabled = true
 		player.can_fix_color = tool_fix_color
+		
 		position = player.get_child(0).get_child(0).global_position
 		smoothed_mouse_pos = lerp(smoothed_mouse_pos, get_global_mouse_position(), 0.3)
 		look_at(smoothed_mouse_pos)
@@ -32,16 +36,24 @@ func _physics_process(delta):
 		linear_velocity = Vector2.ZERO
 		
 		if Input.is_action_just_pressed('throw'):
+			player.tool_count -= 1
+			player.can_fix_color = ''
 			picked_up = false
 			var trow_dir = player.global_position.direction_to(get_global_mouse_position())
 			apply_impulse(trow_dir * THROW_STR)
 		
 		if Input.is_action_just_pressed('drop_item') || player.player_dead:
+			player.tool_count -= 1
+			player.can_fix_color = ''
 			picked_up = false
 			apply_impulse(Vector2(0, 1))
 	else:
 		collision_shape.disabled = false
-		player.can_fix_color = ''
+	
+	if player.tool_count < 0 || player.tool_count > 1:
+		apply_impulse(Vector2(0, 1))
+		player.tool_count = 0
+		picked_up = false
 
 func _on_pick_up_detect_body_entered(body):
 	can_be_picked_up = true
