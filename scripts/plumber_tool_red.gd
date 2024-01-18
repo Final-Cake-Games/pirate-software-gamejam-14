3b58c92fbc52adfa0493d22621cc02e1ddfd8faf
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-18 16:28:40 +0000

    Changed collision masks and script name

--- scripts/plumber_tool_red.gd
+++ scripts/plumber_tool_red.gd
@@ -16,8 +16,9 @@ func _ready():
 
 func _process(delta):
 	if can_be_picked_up:
-		if Input.is_action_just_pressed('pick_up') && !player.player_dead:
+		if Input.is_action_just_pressed('pick_up') && !player.player_dead && player.can_fix_color == '':
 			picked_up = true
+			player.tool_count += 1
 
 func _physics_process(delta):
 	can_kill = (linear_velocity.x > 100 || linear_velocity.y > 100 || linear_velocity.x < -100 || linear_velocity.y < -100)
@@ -32,16 +33,24 @@ func _physics_process(delta):
 		linear_velocity = Vector2.ZERO
 		
 		if Input.is_action_just_pressed('throw'):
+			player.tool_count -= 1
 			picked_up = false
+			player.can_fix_color = ''
 			var trow_dir = player.global_position.direction_to(get_global_mouse_position())
 			apply_impulse(trow_dir * THROW_STR)
 		
 		if Input.is_action_just_pressed('drop_item') || player.player_dead:
+			player.tool_count -= 1
 			picked_up = false
+			player.can_fix_color = ''
 			apply_impulse(Vector2(0, 1))
 	else:
 		collision_shape.disabled = false
-		player.can_fix_color = ''
+	
+	if player.tool_count < 0 || player.tool_count > 1:
+		apply_impulse(Vector2(0, 1))
+		player.tool_count = 0
+		picked_up = false
 
 func _on_pick_up_detect_body_entered(body):
 	can_be_picked_up = true
