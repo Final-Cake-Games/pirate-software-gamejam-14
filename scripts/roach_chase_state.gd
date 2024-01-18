d7ed092d32d762691637bb70ccdb81b8dd657640
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-18 12:47:40 +0000

    Enemy can jump when chasing player
    When he has an obstacle or the player is above him

--- scripts/roach_chase_state.gd
+++ scripts/roach_chase_state.gd
@@ -5,13 +5,14 @@ signal lost_player
 signal fight_player
 signal roach_die
 
-var player_dir_x : float
+@export var JUMP_FORCE : float = 200
 
 func _ready():
 	set_physics_process(false)
 
 func _enter_state() -> void:
 	set_physics_process(true) 
+	
 	animator.play('walk')
 	vessel.player_target = vessel.detection_area.get_overlapping_bodies()[0]
 
@@ -19,14 +20,20 @@ func _exit_state():
 	set_physics_process(false)
 	
 func _physics_process(delta):
-	#print(player_dir_x)  # Sometimes -0 or 0 due to player being above
-	player_dir_x = round((vessel.global_position.direction_to(vessel.player_target.global_position)).x)
-	vessel.velocity.x = move_toward(vessel.velocity.x, player_dir_x * vessel.CHASE_SPEED, 2)
 	vessel.move_and_slide()
+	vessel.velocity.x = move_toward(vessel.velocity.x, vessel.player_dir.x * vessel.CHASE_SPEED, 2)
+	
+	if !vessel.is_on_floor():  # Temp add gravity in roam, make falling state soon
+		vessel.velocity.y += vessel.GRAVITY * delta
+		
+	if ((vessel.is_on_wall() && vessel.is_on_floor()) || (vessel.player_dir.y == -1 && vessel.is_on_floor())):
+		jump()
+
+func jump():
+	vessel.velocity.y = -JUMP_FORCE
 
 func _on_detection_range_body_exited(body):
 	if !vessel.dead:
-		vessel.last_known_player_location = player_dir_x
 		lost_player.emit()
 
 func _on_fight_range_body_entered(body):
