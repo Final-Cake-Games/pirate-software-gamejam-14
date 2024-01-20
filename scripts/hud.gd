8f305e583049e88ef1792f26f63839171cb4c09d
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-20 15:32:59 +0000

    Added HUD elements for valve tracking
    And script to update values from level

---
+++ scripts/hud.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+func set_red_left(amount):
+	$ValvesRemaining/RedAmount.text = str(amount)
+
+func set_blue_left(amount):
+	$ValvesRemaining/BlueAmount.text = str(amount)
+	
+func set_lime_left(amount):
+	$ValvesRemaining/LimeAmount.text = str(amount)
+	
+func set_orange_left(amount):
+	$ValvesRemaining/OrangeAmount.text = str(amount)
