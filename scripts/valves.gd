a8006f7b07fa6453ab00c7e01b3860744a2389b0
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-22 10:03:08 +0000

    Instantiated closing sfx on all valves
    Play while fixing.

--- scripts/valves.gd
+++ scripts/valves.gd
@@ -4,6 +4,7 @@ extends Area2D
 @export_enum('RED', 'BLUE', 'ORANGE', 'LIME') var fix_color : String
 @export var associated_sewage : CPUParticles2D = null
 
+@onready var valve_fixing_sfx = $valve_fixing_sfx
 @onready var animation : AnimatedSprite2D = $AnimatedSprite2D
 @onready var progress_time : Timer = $Progress
 
@@ -13,6 +14,7 @@ var is_fixed : bool = false
 func _ready():
 	progress_time.wait_time = FIX_TIME
 	progress_time.start()
+	valve_fixing_sfx.play()
 
 func _process(_delta):
 	if is_getting_fixed && !is_fixed:	
@@ -26,12 +28,15 @@ func _process(_delta):
 		idle()	
 	
 func get_fixed():
+	
+	valve_fixing_sfx.stream_paused = false
 	animation.play('closing')
 	progress_time.paused = false
 	$ValveProgress._set_value((1 - (progress_time.time_left / FIX_TIME)) * 100)
 	
 func idle():
 	animation.play('default')
+	valve_fixing_sfx.stream_paused = true
 
 func _on_progress_timeout():
 	is_fixed = true
