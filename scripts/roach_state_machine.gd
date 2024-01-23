6f0093e083c5e0aca6e305aa7b71ecb1c6ef3a4b
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-23 22:53:19 +0000

    Separated default sate from current state
    Better readability

--- scripts/roach_state_machine.gd
+++ scripts/roach_state_machine.gd
@@ -2,13 +2,17 @@
 class_name RoachStateMachine
 extends Node
 
-@export var current_state : State  # Only scenes that are of State class or extended from State class 
+@export var default_state : State
+
+var current_state : State  # Only scenes that are of State class or extended from State class 
+
 
 func _ready():
-	change_state(current_state)  # Initialize first state on load
+	change_state(default_state)  # Initialize first state on load
 	
 func change_state(new_state : State):
 	if current_state != null:  # Exit currently loaded state (if there is any...)
 		current_state._exit_state()
 	new_state._enter_state()
 	current_state = new_state  # Update the current state to the just loaded state
+
