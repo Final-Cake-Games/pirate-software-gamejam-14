f56d951d41b98a3e77f0b903f8b1f4edb85dd95e
Eusébio Alves <eusebio.a95@hotmail.com>
2024-01-25 12:18:30 +0000

    Gave gravity to die state to fix hover death

--- scripts/roach_die_state.gd
+++ scripts/roach_die_state.gd
@@ -5,6 +5,7 @@ func _ready():
 	set_physics_process(false)
 
 func _enter_state():
+	set_physics_process(true)
 	vessel.dead = true
 	vessel.velocity = Vector2.ZERO
 	SfxHandler.play_sfx(vessel.HITS.pick_random(), vessel, -1)
@@ -13,4 +14,10 @@ func _enter_state():
 	await animator.animation_finished
 	await get_tree().create_timer(3).timeout
 	vessel.queue_free()
+
+func _physics_process(delta):
+	vessel.move_and_slide()
+	if !vessel.is_on_floor():
+		vessel.velocity.y += vessel.GRAVITY
+	
 
